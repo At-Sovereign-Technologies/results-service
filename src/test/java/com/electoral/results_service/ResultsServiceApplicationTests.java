b10124d50aabe1268f3c2566c5603cd6142159d9
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-04-16 18:42:25 -0500

    first commit

---
+++ src/test/java/com/electoral/results_service/ResultsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electoral.results_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
