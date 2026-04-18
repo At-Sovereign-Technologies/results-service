f24f3a92c109614f9a473e0b89e77d198c013e72
Juan Martín Sánchez <140517935+Palmxl@users.noreply.github.com>
2026-04-17 21:07:31 -0500

    Refactor SQL seed data insertion using SELECT

--- src/main/resources/db/migration/V2__seed.sql
+++ src/main/resources/db/migration/V2__seed.sql
@@ -1,3 +1,19 @@
-INSERT INTO result (election_id, candidate_name, votes) VALUES
-(1, 'Candidato A', 600),
-(1, 'Candidato B', 400);
+INSERT INTO result (election_id, candidate_name, votes)
+SELECT 
+    e_id,
+
+    (
+        ARRAY[
+            'Juan Pérez',
+            'María Gómez',
+            'Carlos Rodríguez',
+            'Ana Martínez',
+            'Luis Fernández',
+            'Sofía Ramírez'
+        ]
+    )[c_id] AS candidate_name,
+
+    (floor(random() * 8000) + 1000)::int
+
+FROM generate_series(1, 3) e_id,   -- 3 elecciones
+     generate_series(1, 6) c_id;   -- 6 candidatos
